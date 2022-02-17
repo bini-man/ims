3cf90041ae4a9c3e9aec0dc93d4e126ec7cea174
bini-man <biniyambadege4@gmail.com>
2022-02-17 17:06:05 +0300

    authenticating user finished

--- server/route/route.js
+++ server/route/route.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken')
 const verify = require('./Verify')
 
 // for createing incident
-route.post("/incident_creat", verify,async (req,res)=>{
+route.post("/incident_creat", async (req,res)=>{
     const user_id=req.user._id;
     const created_by_d="";
     User.findById({_id:user_id},(async function(err,result){
@@ -29,7 +29,7 @@ route.post("/incident_creat", verify,async (req,res)=>{
    
 })
 // for creating user
-route.post("/user_creat",  verify, async (req,res)=>{
+route.post("/user_creat",   async (req,res)=>{
     const email_check=await User.findOne({email:req.body.email})
     if(email_check) return res.send("email alredy exists")
     const salt= await bcrypt.genSalt(10)
@@ -50,7 +50,7 @@ route.post("/user_creat",  verify, async (req,res)=>{
     }
 })
 // to see all incident
-route.get('/all_user', verify,(req,res)=>{
+route.get('/all_user', (req,res)=>{
    
     User.find({},(function(err,result){
         if(err) return res.send(err)
@@ -59,7 +59,7 @@ route.get('/all_user', verify,(req,res)=>{
     )
 })
 // to see all user
-route.get('/all_incident', verify,(req,res)=>{
+route.get('/all_incident', (req,res)=>{
     Incident.find({},(function(err,result){
         if(err) return res.send(err)
         res.json(result)
@@ -67,7 +67,7 @@ route.get('/all_incident', verify,(req,res)=>{
     )
 })
 // to return specific user
-route.get('/user/:id', verify,(req,res)=>{
+route.get('/user/:id',(req,res)=>{
     const user_id= req.params.id;
     User.findById({_id:user_id},(function(err,result){
         if(err) return res.send(err)
@@ -75,7 +75,7 @@ route.get('/user/:id', verify,(req,res)=>{
     }))
 })
 // to return specific incident
-route.get('/incident/:id',  verify ,(req,res)=>{
+route.get('/incident/:id',  (req,res)=>{
     const incident_id= req.params.id;
     Incident.findById({_id:incident_id},(function(err,result){
         if(err) return res.send(err)
@@ -83,7 +83,7 @@ route.get('/incident/:id',  verify ,(req,res)=>{
     }))
 })
 // to Delete specific user
-route.post('/user/:id',  verify,(req,res)=>{
+route.post('/user/:id',  (req,res)=>{
     const user_id= req.params.id;
     User.findByIdAndDelete({_id:user_id},(function(err,result){
         if(err) return res.send(err)
@@ -91,7 +91,7 @@ route.post('/user/:id',  verify,(req,res)=>{
     }))
 })
 // to Delete specific incident
-route.post('/incident/:id', verify,(req,res)=>{
+route.post('/incident/:id', (req,res)=>{
     const incident_id= req.params.id;
     Incident.findByIdAndDelete({_id:incident_id},(function(err,result){
         if(err) return res.send(err)
@@ -99,7 +99,7 @@ route.post('/incident/:id', verify,(req,res)=>{
     }))
 })
 // to update specific user information
-route.post('/update_user/:id', verify,async (req,res)=>{
+route.post('/update_user/:id', async (req,res)=>{
     const user_id= req.params.id;
     const salt= await bcrypt.genSalt(10)
     const hashPassword= await bcrypt.hash(req.body.password,salt)
@@ -116,7 +116,7 @@ route.post('/update_user/:id', verify,async (req,res)=>{
     }))
 })
 // to update specific incident information
-route.post('/update_incident/:id', verify,(req,res)=>{
+route.post('/update_incident/:id', (req,res)=>{
     const incident_id= req.params.id;
     Incident.findByIdAndUpdate({_id:incident_id},{
         incident_name:req.body.name,
@@ -134,12 +134,12 @@ route.post('/login',async (req,res)=>{
     const user=await User.findOne({email:req.body.email})
     if(!user) return res.send("email not exists")
     const valid_password= await bcrypt.compare(req.body.password,user.password)
-    if(!valid_password) return res.status(400).send("invalid password")
+    if(!valid_password) return res.send("invalid password")
     const token= jwt.sign({_id:user._id},process.env.TOKEN_SECRET)
-    res.header('auth-token',token).send(token);
+    res.header('auth-token',token).send('token');
 })
 // to assign incidents to users
-route.post('/assign_incident/:id', verify,(req,res)=>{
+route.post('/assign_incident/:id', (req,res)=>{
     const incident_id= req.params.id;
     const user_id=req.user._id
     User.findById({_id:user_id},(async function(err,result){
@@ -157,7 +157,7 @@ route.post('/assign_incident/:id', verify,(req,res)=>{
 }))
 })
 // incident's created by user's or assigned to them
-route.get('/created_assigned', verify,(req,res)=>{
+route.get('/created_assigned', (req,res)=>{
     const user_id=req.user._id
    
     User.findById({_id:user_id},( async function(err,result){
--- server/server.js
+++ server/server.js
@@ -3,11 +3,14 @@ const app=express();
 const mongoose = require('mongoose')
 const route=require('./route/route')
 require("dotenv").config();
+var cors = require('cors')
+
+app.use(cors()) 
 mongoose.connect('mongodb+srv://bini-man:biniMAN123..@cluster0.tcrqx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 () => console.log('connected to db')
 );
 app.use(express.json())
 app.use('/api',route)
-app.listen(3000, ()=>{
+app.listen(3001, ()=>{
     console.log("server started")
 })
