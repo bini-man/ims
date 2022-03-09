e09a3deac597eb0852aa2f1f2351cc0f90c96842
bini-man <biniyambadege4@gmail.com>
2022-03-09 16:42:08 +0300

    minor fix on admin dashboard

--- client/src/component/AdminDashboard.js
+++ client/src/component/AdminDashboard.js
@@ -10,7 +10,6 @@ import { useLocation } from 'react-router-dom'
 function AdminDashboard() {
     const drawerwidth = 260
 const [item,setItem]=useState([])
-    
 const navigate=useNavigate()
 const location=useLocation()
 const useStyles=makeStyles({
